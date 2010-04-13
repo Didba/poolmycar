d93fcb29605650ec0895b95f3f7a64e4dd7406ee
alberto.rapalino <alberto.rapalino@f3317bc4-e7ff-11de-85bf-c3887d924cc8>
2010-04-13 15:04:26 +0000

    1 cascade

--- PoolMyCarCode/PoolMyCarCode-ejb/src/java/viaggi/Viaggio.java
+++ PoolMyCarCode/PoolMyCarCode-ejb/src/java/viaggi/Viaggio.java
@@ -8,6 +8,7 @@ package viaggi;
 import java.io.Serializable;
 import java.util.Calendar;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -30,7 +31,7 @@ public class Viaggio implements Serializable {
     @Temporal(javax.persistence.TemporalType.DATE)
     private Calendar dataPartenza;
     private float lunghezzaPercorso;
-    @OneToMany
+    @OneToMany(cascade=CascadeType.ALL)
     private List<Tappa> tappeIntermedie;
     @ManyToMany
     private List<Viaggiatore> viaggiatori;
