93cdcf8f1e80f2c694d82eddcd066cb18a4fe506
erica.turin <erica.turin@f3317bc4-e7ff-11de-85bf-c3887d924cc8>
2010-04-15 12:30:18 +0000

    Refactoring di GestoreViaggiBeanBean con GestoreViaggiBeans

--- trunk/PoolMyCarCode/PoolMyCarCode-ejb/src/java/ejb/GestoreViaggiBean.java
+++ trunk/PoolMyCarCode/PoolMyCarCode-ejb/src/java/ejb/GestoreViaggiBean.java
@@ -20,25 +20,31 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 import utenti.Autista;
 import utenti.Indirizzo;
 import viaggi.Bacheca;
 import viaggi.Pacchetto;
 import viaggi.Tappa;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 /**
  *
  * @author berto
  */
 @Stateless
 public class GestoreViaggiBean implements GestoreViaggiBeanLocal {
-    @EJB
+   @EJB
     private TappaFacadeLocal tappaFacade;
     @EJB
     private ViaggioFacadeLocal viaggioFacade;
     @EJB
     private PacchettoFacadeLocal pacchettoFacade;
-    
+
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method" or "Web Service > Add Operation")
 
@@ -80,15 +86,14 @@ public class GestoreViaggiBean implements GestoreViaggiBeanLocal {
 
         tappa.setLatitudine(latlon[0]);
         tappa.setLongitudine(latlon[1]);
-        Indirizzo indi = new Indirizzo();
-        indi.setVia(indirizzo);
+        Indirizzo indi = reverseGeocoding(latlon[0],latlon[1]);
         tappa.setIndirizzo(indi);
         //TO-DO:crea oggetto indirizzo parsificando la string e aggiungilo a tappa
         return tappa;
 
     }
-    
-    
+
+
     public void inserisciPacchetto(List<Tappa> tappe, List<Calendar> date, Autista autista, String nota, boolean richiestaContributi) throws IllegalStateException
     {
 
@@ -99,7 +104,7 @@ public class GestoreViaggiBean implements GestoreViaggiBeanLocal {
             throw new IllegalArgumentException("inserisci almeno una data");
         if(autista==null)
             throw new IllegalArgumentException("manca autista");
-        
+
 
         //TO-DO: facade della bacheca
 
@@ -126,9 +131,73 @@ public class GestoreViaggiBean implements GestoreViaggiBeanLocal {
 
     }
 
-    public RisultatiRicercaViaggi ricercaViaggi(String partenza, String arrivo, boolean intervallo, Date data1, Date data2, Date dataOraPartenza) {
-        return null;
+    @SuppressWarnings("empty-statement")
+    private Indirizzo reverseGeocoding(double lat, double lon) {
+        //http://www.java-tips.org/java-se-tips/javax.xml.parsers/how-to-read-xml-file-in-java.html
+        //http://code.google.com/intl/it-IT/apis/maps/documentation/geocoding/index.html#XML
+        Indirizzo ris=new Indirizzo();
+        URL url=null;
+        try {
+            url = new URL("http://maps.google.com/maps/api/geocode/xml?latlng="+lat+","+lon+"&sensor=false");
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(GestoreViaggiBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        BufferedReader in;
+        try {
+            in = new BufferedReader(new InputStreamReader(url.openStream()));
+            String inputLine;
+            String buffer="";
+            while ((inputLine = in.readLine()) != null){
+                buffer=buffer+inputLine;
+            }
+
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(buffer);;
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeLst = doc.getElementsByTagName("result");
+
+
+            for (int s = 0; s < nodeLst.getLength(); s++) {
+
+                Node fstNode = nodeLst.item(s);
+
+                if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+/*
+ * FARE IL PARSING DELL'XML
+                  Element fstElmnt = (Element) fstNode;
+                  NodeList fstNmElmntLst = fstElmnt.getElementsByTagName("firstname");
+                  Element fstNmElmnt = (Element) fstNmElmntLst.item(0);
+                  NodeList fstNm = fstNmElmnt.getChildNodes();
+                  System.out.println("First Name : "  + ((Node) fstNm.item(0)).getNodeValue());
+                  NodeList lstNmElmntLst = fstElmnt.getElementsByTagName("lastname");
+                  Element lstNmElmnt = (Element) lstNmElmntLst.item(0);
+                  NodeList lstNm = lstNmElmnt.getChildNodes();
+                  System.out.println("Last Name : " + ((Node) lstNm.item(0)).getNodeValue());
+ */
+
+                }
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(GestoreViaggiBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (ParserConfigurationException ex) {
+            Logger.getLogger(GestoreViaggiBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (SAXException ex) {
+            Logger.getLogger(GestoreViaggiBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+        return ris;
     }
 
-
+    public RisultatiRicercaViaggi ricercaViaggi(String partenza, String arrivo, boolean intervallo, Date data1, Date data2, Date dataOraPartenza) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
