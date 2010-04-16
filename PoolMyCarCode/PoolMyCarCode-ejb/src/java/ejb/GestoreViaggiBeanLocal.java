00398cd145252a869b82ea9240512a42b41e264b
erica.turin <erica.turin@f3317bc4-e7ff-11de-85bf-c3887d924cc8>
2010-04-16 13:01:39 +0000

    Salvataggio distanza del viaggio, aggiunto parametro lunghezzaPercorso a Pacchetto, continua la definizione dei metodi per la ricerca

--- PoolMyCarCode/PoolMyCarCode-ejb/src/java/ejb/GestoreViaggiBeanLocal.java
+++ PoolMyCarCode/PoolMyCarCode-ejb/src/java/ejb/GestoreViaggiBeanLocal.java
@@ -17,9 +17,10 @@ import viaggi.Tappa;
  */
 public interface GestoreViaggiBeanLocal {
     public Tappa geocoding(String indirizzo);
-    public void inserisciPacchetto(List<Tappa> tappe, List<Calendar> date, Autista autista, String nota, boolean richiestaContributi) throws IllegalStateException;
 
    RisultatiRicercaViaggi ricercaViaggi(String partenza, String arrivo, boolean intervallo, Date data1, Date data2, Date dataOraPartenza);
+
+    public void inserisciPacchetto(List<Tappa> tappe, List<Calendar> date, Autista autista, String nota, boolean richiestaContributi,String distanza) throws IllegalStateException;
 
 
 }
