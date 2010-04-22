3beda7261ceb3c52e988bb4b02e1a129ccdeee38
erica.turin <erica.turin@f3317bc4-e7ff-11de-85bf-c3887d924cc8>
2010-04-22 15:48:31 +0000

    Cambiata la visualizzazione dei viaggi trovati, negli entity i campi di data sono diventati di tipo timestamp( così c'è anche l'ora)

--- PoolMyCarCode/PoolMyCarCode-ejb/src/java/ejb/RisultatiRicercaViaggi.java
+++ PoolMyCarCode/PoolMyCarCode-ejb/src/java/ejb/RisultatiRicercaViaggi.java
@@ -21,6 +21,26 @@ public class RisultatiRicercaViaggi implements RisultatiRicercaViaggiLocal {
     // "Insert Code > Add Business Method")
     private List<Pacchetto> pacchetti = null;
     protected int pacchettiVisti=0;
+    protected int numGruppoPacchetti;
+    private int numUltimoGruppo=0;
+
+    /**
+     * Get the value of numGruppoPacchetti
+     *
+     * @return the value of numGruppoPacchetti
+     */
+    public int getNumGruppoPacchetti() {
+        return numGruppoPacchetti;
+    }
+
+    /**
+     * Set the value of numGruppoPacchetti
+     *
+     * @param numGruppoPacchetti new value of numGruppoPacchetti
+     */
+    public void setNumGruppoPacchetti(int numGruppoPacchetti) {
+        this.numGruppoPacchetti = numGruppoPacchetti;
+    }
 
     /**
      * Get the value of pacchettiVisti
@@ -58,20 +78,47 @@ public class RisultatiRicercaViaggi implements RisultatiRicercaViaggiLocal {
     public void setPacchetti(List<Pacchetto> pacchetti) {
         this.pacchetti = pacchetti;
     }
-//motodo per restituire i successivi n (al massimo n) pacchetti
-    public List<Pacchetto> getNextPacchetti(int n){
+//motodo per restituire i successivi numGruppoPacchetti (al massimo numGruppoPacchetti) pacchetti
+    public List<Pacchetto> getNextPacchetti(){
+
         if((pacchetti == null)||(pacchetti.size()<=pacchettiVisti))
             return null;
         else{
             List<Pacchetto> l= new LinkedList<Pacchetto>();
-            int i = pacchettiVisti;
-            while(i<= pacchetti.size()  && (pacchettiVisti+n)> i){
-                l.add(pacchetti.get(i));
-                i++;
-            }
+            int n = numGruppoPacchetti;
+            if(pacchettiVisti+numGruppoPacchetti>= pacchetti.size())
+                n = pacchetti.size()-pacchettiVisti-1;
+
+            for(int i = 0 ; i<n;i++)
+                l.add(pacchetti.get(pacchettiVisti+i));
+            
             pacchettiVisti+=n;
+            numUltimoGruppo=n;
             return l;
         }
+    }
+//motodo per restituire i precedenti numGruppoPacchetti  pacchetti
+    public List<Pacchetto> getPredPacchetti(){
+        
+        if((pacchetti == null)||(pacchettiVisti-numGruppoPacchetti<=0))
+            return null;
+        else{
+            List<Pacchetto> l= new LinkedList<Pacchetto>();
+            int n = numGruppoPacchetti;
+           for(int i = 0;i<n;i++){
+                l.add(pacchetti.get(pacchettiVisti-numUltimoGruppo-1-i));
+           }
+            pacchettiVisti-=numUltimoGruppo;
+            numUltimoGruppo=numGruppoPacchetti;
+            return l;
+        }
+    }
+
+    public boolean avanti(){
+        return pacchettiVisti<pacchetti.size();
+    }
+    public boolean indietro(){
+        return pacchettiVisti>numGruppoPacchetti;
     }
 
    
