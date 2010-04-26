0dd5b9c186064e9765aff02fc8ffd7671b6bdc0c
alberto.rapalino <alberto.rapalino@f3317bc4-e7ff-11de-85bf-c3887d924cc8>
2010-04-26 13:12:17 +0000

    junit

--- trunk/PoolMyCarCode/PoolMyCarCode-ejb/test/ejb/GestoreViaggiBeanTest.java
+++ trunk/PoolMyCarCode/PoolMyCarCode-ejb/test/ejb/GestoreViaggiBeanTest.java
@@ -6,24 +6,15 @@
 package ejb;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.LinkedList;
 import java.util.List;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import utenti.CommentoAutista;
-import utenti.CommentoViaggiatore;
 import utenti.Indirizzo;
 import utenti.Viaggiatore;
-import viaggi.Bacheca;
 import viaggi.Pacchetto;
-import viaggi.Richiesta;
 import viaggi.Tappa;
-import viaggi.Viaggio;
 
 /**
  *
@@ -42,31 +33,18 @@ public class GestoreViaggiBeanTest {
     public static void tearDownClass() throws Exception {
     }
 
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of geocoding method, of class GestoreViaggiBean.
      */
     @Test
     public void testGeocoding() {
         System.out.println("geocoding");
-        String indirizzo = "Torino, Corso Svizzera 185, Italy";
+        String indirizzo = "";
         GestoreViaggiBean instance = new GestoreViaggiBean();
-        
-        Tappa expResult = new Tappa();
-        expResult.setLatitudine(45.0904249);
-        expResult.setLongitudine(7.6607690);
-        
+        Tappa expResult = null;
         Tappa result = instance.geocoding(indirizzo);
-        result.setIndirizzo(null);
-        assertEquals(result, expResult);
-        
+        //assertEquals("", "");
+        assertTrue(true);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -77,54 +55,31 @@ public class GestoreViaggiBeanTest {
     @Test
     public void testInserisciPacchetto() {
         System.out.println("inserisciPacchetto");
-        Tappa t1=new Tappa();
-        Indirizzo i1=new Indirizzo();
-        i1.setCitta("Torino");
-        t1.setIndirizzo(i1);
-        Tappa t2=new Tappa();
-        Indirizzo i2=new Indirizzo();
-        i2.setCitta("Milano");
-        t2.setIndirizzo(i1);
-        List<Tappa> tappe = new LinkedList<Tappa>();
-        tappe.add(t1);
-        tappe.add(t2);
-        List<Calendar> date = new LinkedList<Calendar>();
-        Calendar c = new GregorianCalendar(2010, 04, 10);
-        date.add(c);
-        Viaggiatore autista = new Viaggiatore();
-        autista.setLogin("autista");
-        autista.setPassword("password");
-        autista.setAutista(true);
-        String nota = "Nota";
+        List<Tappa> tappe = null;
+        List<Calendar> date = null;
+        Viaggiatore autista = null;
+        String nota = "";
         boolean richiestaContributi = false;
-        String distanza = "10000";
+        String distanza = "";
         GestoreViaggiBean instance = new GestoreViaggiBean();
-        Pacchetto expResult = new Pacchetto();
-        expResult.setPartenza(t1);
-        expResult.setArrivo(t2);
-        expResult.setTappeIntermedie(new LinkedList<Tappa>());
-        expResult.setBacheca(new Bacheca());
-        expResult.setAutista(autista);
-        expResult.setCommentiAutista(new LinkedList<CommentoAutista>());
-        expResult.setCommentiViaggiatori(new LinkedList<CommentoViaggiatore>());
-        expResult.setFine(c);
-        expResult.setInizio(c);
-        expResult.setLunghezzaPercorso(10000);
-        expResult.setNota(nota);
-        expResult.setRichiestaContributi(false);
-        expResult.setTappeIntermedie(new LinkedList<Tappa>());
-        Viaggio v=new Viaggio();
-        v.setPartenza(t1);
-        v.setArrivo(t2);
-        v.setPacchetto(expResult);
-        v.setDataPartenza(c);
-        v.setLunghezzaPercorso(10000);
-        v.setViaggiatori(new LinkedList<Viaggiatore>());
-        v.setRichieste(new LinkedList<Richiesta>());
-        v.setTappeIntermedie(new LinkedList<Tappa>());
-        List<Viaggio> viaggi= new LinkedList<Viaggio>();
-        expResult.setViaggi(viaggi);
+        Pacchetto expResult = null;
         Pacchetto result = instance.inserisciPacchetto(tappe, date, autista, nota, richiestaContributi, distanza);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of reverseGeocoding method, of class GestoreViaggiBean.
+     */
+    @Test
+    public void testReverseGeocoding() {
+        System.out.println("reverseGeocoding");
+        double lat = 0.0;
+        double lon = 0.0;
+        GestoreViaggiBean instance = new GestoreViaggiBean();
+        Indirizzo expResult = null;
+        Indirizzo result = instance.reverseGeocoding(lat, lon);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
@@ -160,6 +115,21 @@ public class GestoreViaggiBeanTest {
         GestoreViaggiBean instance = new GestoreViaggiBean();
         Pacchetto expResult = null;
         Pacchetto result = instance.aggiornaPacchetto(pacchetto);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCitta method, of class GestoreViaggiBean.
+     */
+    @Test
+    public void testGetCitta() {
+        System.out.println("getCitta");
+        String subCitta = "";
+        GestoreViaggiBean instance = new GestoreViaggiBean();
+        List expResult = null;
+        List result = instance.getCitta(subCitta);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
