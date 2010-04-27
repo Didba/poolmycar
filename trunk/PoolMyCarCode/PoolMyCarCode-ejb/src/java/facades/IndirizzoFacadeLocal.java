245f05936ee01f4ecbc28e219ecd2e642afca15b
alberto.rapalino <alberto.rapalino@f3317bc4-e7ff-11de-85bf-c3887d924cc8>
2010-04-27 11:53:44 +0000

    qualche javadoc

--- trunk/PoolMyCarCode/PoolMyCarCode-ejb/src/java/facades/IndirizzoFacadeLocal.java
+++ trunk/PoolMyCarCode/PoolMyCarCode-ejb/src/java/facades/IndirizzoFacadeLocal.java
@@ -26,8 +26,15 @@ public interface IndirizzoFacadeLocal {
 
     List<Indirizzo> findAll();
 
+    /** Restituisce tutte le stringe "citta" che iniziano con il parametro
+     * @param subCitta la stringa da cercare
+     * @return una lista di stringhe che rappresentano i nomi delle citta presenti su db
+     */
     public List<String> getCitta(String subCitta);
 
+    /** carica tutti i nomi della città presenti nel database
+     * @return la lista dei nomi delle città sotto forma di String
+     */
     public java.util.List<java.lang.String> getCitta();
 
 }
